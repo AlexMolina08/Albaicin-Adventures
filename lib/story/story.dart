5e8acc1d1c02d8d6dafb5bb37cd013c2fb34485e
alex <alexmolina@gmail.com>
2020-09-12 17:40:18 +0200

    story class created

---
+++ lib/story/story.dart
@@ -0,0 +1,9 @@
+class Story{
+  String _text , _choice1 , _choice2;
+  Story(this._text , this._choice1 , this._choice2);
+
+  String getText() => this._text;
+  String getChoice1() => this._choice1;
+  String getChoice2() => this._choice2;
+
+}
